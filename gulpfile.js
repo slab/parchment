d9576a1bbe446d0715971cb01284145dadcd2623
Jason Chen <jhchen7@gmail.com>
2015-05-06 14:00:03 -0700

    use tsify

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,23 @@
 var browserify = require('browserify');
-var buffer = require('vinyl-buffer');
-var flatten = require('gulp-flatten');
 var gulp = require('gulp');
 var karma = require('karma').server;
 var source = require('vinyl-source-stream');
-var ts = require('gulp-typescript');
+var tsify = require('tsify');
 
 
 gulp.task('default', ['build']);
 
-gulp.task('build', ['build:browserify']);
 
-gulp.task('build:ts', function() {
-  var compiler = ts({
-    declarationFiles: false,
-    target: 'es5',
-    module: 'commonjs'
-  });
-  return gulp.src(['src/**/*.ts', 'test/parchment.ts'], { base: '.' })
-    .pipe(buffer())
-    .pipe(compiler).js
-    .pipe(gulp.dest('./.build/'));
-});
-
-gulp.task('build:browserify', ['build:browserify:src', 'build:browserify:test']);
-
-gulp.task('build:browserify:src', ['build:ts'], function() {
-  var b = browserify({
-    entries: './.build/src/parchment.js',
-    standalone: 'Parchment'
-  });
-  return b.bundle()
-    .pipe(source('./.build/src/parchment.js'))
-    .pipe(buffer())
-    .pipe(flatten())
+gulp.task('build', function() {
+  return bundler('./src/parchment.ts')
+    .pipe(source('parchment.js'))
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('build:browserify:test', ['build:ts'], function() {
-  var b = browserify('./.build/test/parchment.js');
-  return b.bundle()
-    .pipe(source('./.build/test/parchment.exposed.js'))
-    .pipe(buffer())
-    .pipe(gulp.dest('./'));
+gulp.task('build:test', ['build:ts'], function() {
+  return bundler('./test/parchment.ts')
+    .pipe(source('parchment.js'))
+    .pipe(gulp.dest('./build/test'));
 });
 
 gulp.task('test', function(done) {
@@ -61,3 +36,11 @@ gulp.task('test:server', function(done) {
 gulp.task('watch', function() {
   gulp.watch('**/*.ts', ['build']);
 });
+
+
+function bundler(files) {
+  return browserify({
+    standalone: 'Parchment',
+    entries: files
+  }).plugin('tsify', { target: 'ES5' }).bundle();
+}
