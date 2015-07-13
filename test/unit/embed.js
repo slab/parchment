aa0b112b7565520318a28ac3b0621cfd177d352e
Jason Chen <jhchen7@gmail.com>
2015-07-13 14:45:34 -0700

    add embed tests

--- test/unit/embed.js
+++ test/unit/embed.js
@@ -1,3 +1,16 @@
 describe('EmbedBlot', function() {
+  it('getValue()', function() {
+    var imageBlot = Registry.create('image');
+    expect(imageBlot.getValue()).toEqual({ image: true });
+  });
 
+  it('insertAt()', function() {
+    var container = Registry.create('inline');
+    var imageBlot = Registry.create('image');
+    container.appendChild(imageBlot);
+    imageBlot.insertAt(0, 'image', true);
+    expect(container.children.head.getValue()).toEqual({ image: true });
+    imageBlot.insertAt(1, '!');
+    expect(container.children.tail.getValue()).toEqual('!');
+  });
 });
