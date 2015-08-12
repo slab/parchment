02fa6ee979b258afb40655fcf78132e66288a9e1
Jason Chen <jhchen7@gmail.com>
2015-08-11 20:09:55 -0700

    remove unused functions

--- src/util.ts
+++ src/util.ts
@@ -1,34 +1,3 @@
-export function copy(source, target = {}) {
-  return Object.keys(source).reduce(function(memo, key) {
-    memo[key] = source[key];
-    return memo;
-  }, target);
-}
-
-export function inherit(ClassObject, SuperClass) {
-  var SubClass = function() {
-    SuperClass.apply(this, arguments);
-  };
-  for (let prop in SuperClass) {
-    if (SuperClass.hasOwnProperty(prop)) {
-      SubClass[prop] = SuperClass[prop];
-    }
-  }
-  var Extender = function() { this.constructor = SubClass; }
-  Extender.prototype = SuperClass.prototype;
-  SubClass.prototype = new Extender();
-  for (let prop in ClassObject) {
-    if (ClassObject.hasOwnProperty(prop)) {
-      if (typeof ClassObject[prop] === 'function') {
-        SubClass.prototype[prop] = ClassObject[prop];
-      } else {
-        SubClass[prop] = ClassObject[prop];
-      }
-    }
-  }
-  return SubClass;
-}
-
 // Shallow object comparison
 export function isEqual(obj1, obj2) {
   if (obj1 === obj2) return true;
@@ -37,10 +6,6 @@ export function isEqual(obj1, obj2) {
     if (obj1[prop] !== obj2[prop]) return false;
   }
   return true;
-}
-
-export function merge(obj1, obj2) {
-  return copy(obj2, copy(obj1));
 }
 
 export function mixin(derivedClass: any, baseClasses: any[]) {
