ed4717f5fe5d31f393e799d873ec0b72add4c46a
Jason Chen <jhchen7@gmail.com>
2015-08-13 15:51:15 -0700

    make node lengths unequal

--- test/unit/root.js
+++ test/unit/root.js
@@ -2,14 +2,14 @@ describe('Root', function() {
   describe('findPath()', function() {
     beforeEach(function() {
       var rootNode = document.createElement('div');
-      rootNode.innerHTML = '<p><strong>Te</strong><em><strong>st</strong></em></p>'
+      rootNode.innerHTML = '<p><strong>012</strong><span>34</span><em><strong>5678</strong></em></p>'
       this.root = new RootBlot(rootNode);
     });
 
     it('boundary', function() {
-      var path = this.root.findPath(2);
+      var path = this.root.findPath(5);
       var expected = [
-        { blotName: 'block', offset: 2 },
+        { blotName: 'block', offset: 5 },
         { blotName: 'italic', offset: 0 },
         { blotName: 'bold', offset: 0 },
         { blotName: 'text', offset: 0 }
@@ -21,12 +21,12 @@ describe('Root', function() {
     });
 
     it('inclusive', function() {
-      var path = this.root.findPath(4);
+      var path = this.root.findPath(9);
       var expected = [
-        { blotName: 'block', offset: 2 },
+        { blotName: 'block', offset: 5 },
         { blotName: 'italic', offset: 0 },
         { blotName: 'bold', offset: 0 },
-        { blotName: 'text', offset: 2 }
+        { blotName: 'text', offset: 4 }
       ];
       path.forEach(function(position, i) {
         expect(position.blot.statics.blotName).toEqual(expected[i].blotName);
