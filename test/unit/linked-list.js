6ede1ac0bf0c0cb72ac23b67c86dbc21c2aa8ae1
Jason Chen <jhchen7@gmail.com>
2015-08-26 20:02:50 -0700

    Use toBe when appropriate
    Technically more correct since we are looking for the exact linked list
    element

--- test/unit/linked-list.js
+++ test/unit/linked-list.js
@@ -12,9 +12,9 @@ describe('LinkedList', function() {
   describe('manipulation', function() {
     it('append to empty list', function() {
       this.list.append(this.a);
-      expect(this.list.length).toEqual(1);
-      expect(this.list.head).toEqual(this.a);
-      expect(this.list.tail).toEqual(this.a);
+      expect(this.list.length).toBe(1);
+      expect(this.list.head).toBe(this.a);
+      expect(this.list.tail).toBe(this.a);
       expect(this.a.prev).toBeUndefined();
       expect(this.a.next).toBeUndefined();
     });
@@ -22,43 +22,43 @@ describe('LinkedList', function() {
     it('insert to become head', function() {
       this.list.append(this.b);
       this.list.insertBefore(this.a, this.b);
-      expect(this.list.length).toEqual(2);
-      expect(this.list.head).toEqual(this.a);
-      expect(this.list.tail).toEqual(this.b);
+      expect(this.list.length).toBe(2);
+      expect(this.list.head).toBe(this.a);
+      expect(this.list.tail).toBe(this.b);
       expect(this.a.prev).toBeUndefined();
-      expect(this.a.next).toEqual(this.b);
-      expect(this.b.prev).toEqual(this.a);
+      expect(this.a.next).toBe(this.b);
+      expect(this.b.prev).toBe(this.a);
       expect(this.b.next).toBeUndefined();
     });
 
     it('insert to become tail', function() {
       this.list.append(this.a);
       this.list.insertBefore(this.b, undefined);
-      expect(this.list.length).toEqual(2);
-      expect(this.list.head).toEqual(this.a);
-      expect(this.list.tail).toEqual(this.b);
+      expect(this.list.length).toBe(2);
+      expect(this.list.head).toBe(this.a);
+      expect(this.list.tail).toBe(this.b);
       expect(this.a.prev).toBeUndefined();
-      expect(this.a.next).toEqual(this.b);
-      expect(this.b.prev).toEqual(this.a);
+      expect(this.a.next).toBe(this.b);
+      expect(this.b.prev).toBe(this.a);
       expect(this.b.next).toBeUndefined();
     });
 
     it('insert in middle', function() {
       this.list.append(this.a, this.c);
       this.list.insertBefore(this.b, this.c);
-      expect(this.list.length).toEqual(3);
-      expect(this.list.head).toEqual(this.a);
-      expect(this.a.next).toEqual(this.b);
-      expect(this.b.next).toEqual(this.c);
-      expect(this.list.tail).toEqual(this.c);
+      expect(this.list.length).toBe(3);
+      expect(this.list.head).toBe(this.a);
+      expect(this.a.next).toBe(this.b);
+      expect(this.b.next).toBe(this.c);
+      expect(this.list.tail).toBe(this.c);
     });
 
     it('remove head', function() {
       this.list.append(this.a, this.b);
       this.list.remove(this.a);
-      expect(this.list.length).toEqual(1);
-      expect(this.list.head).toEqual(this.b);
-      expect(this.list.tail).toEqual(this.b);
+      expect(this.list.length).toBe(1);
+      expect(this.list.head).toBe(this.b);
+      expect(this.list.tail).toBe(this.b);
       expect(this.list.head.prev).toBeUndefined();
       expect(this.list.tail.next).toBeUndefined();
     });
@@ -66,9 +66,9 @@ describe('LinkedList', function() {
     it('remove tail', function() {
       this.list.append(this.a, this.b);
       this.list.remove(this.b);
-      expect(this.list.length).toEqual(1);
-      expect(this.list.head).toEqual(this.a);
-      expect(this.list.tail).toEqual(this.a);
+      expect(this.list.length).toBe(1);
+      expect(this.list.head).toBe(this.a);
+      expect(this.list.tail).toBe(this.a);
       expect(this.list.head.prev).toBeUndefined();
       expect(this.list.tail.next).toBeUndefined();
     });
@@ -76,22 +76,22 @@ describe('LinkedList', function() {
     it('remove inner', function() {
       this.list.append(this.a, this.b, this.c);
       this.list.remove(this.b);
-      expect(this.list.length).toEqual(2);
-      expect(this.list.head).toEqual(this.a);
-      expect(this.list.tail).toEqual(this.c);
+      expect(this.list.length).toBe(2);
+      expect(this.list.head).toBe(this.a);
+      expect(this.list.tail).toBe(this.c);
       expect(this.list.head.prev).toBeUndefined();
       expect(this.list.tail.next).toBeUndefined();
-      expect(this.a.next).toEqual(this.c);
-      expect(this.c.prev).toEqual(this.a);
+      expect(this.a.next).toBe(this.c);
+      expect(this.c.prev).toBe(this.a);
       // Maintain references
-      expect(this.b.prev).toEqual(this.a);
-      expect(this.b.next).toEqual(this.c);
+      expect(this.b.prev).toBe(this.a);
+      expect(this.b.next).toBe(this.c);
     });
 
     it('remove only node', function() {
       this.list.append(this.a);
       this.list.remove(this.a);
-      expect(this.list.length).toEqual(0);
+      expect(this.list.length).toBe(0);
       expect(this.list.head).toBeUndefined();
       expect(this.list.tail).toBeUndefined();
     });
@@ -99,7 +99,7 @@ describe('LinkedList', function() {
     it('empty', function() {
       this.list.append(this.a, this.b);
       this.list.empty();
-      expect(this.list.length).toEqual(0);
+      expect(this.list.length).toBe(0);
       expect(this.list.head).toBeUndefined();
       expect(this.list.tail).toBeUndefined();
     });
@@ -127,7 +127,7 @@ describe('LinkedList', function() {
 
     it('iterate over empty list', function() {
       this.list.forEach(this.spy.callback)
-      expect(this.spy.callback.calls.count()).toEqual(0)
+      expect(this.spy.callback.calls.count()).toBe(0)
     });
 
     it('iterate non-head start', function() {
@@ -136,8 +136,8 @@ describe('LinkedList', function() {
       var b = next();
       var c = next();
       var d = next();
-      expect(b).toEqual(this.b);
-      expect(c).toEqual(this.c);
+      expect(b).toBe(this.b);
+      expect(c).toBe(this.c);
       expect(d).toBeUndefined();
     });
 
@@ -152,19 +152,19 @@ describe('LinkedList', function() {
 
     it('offset', function() {
       this.list.append(this.a, this.b, this.c);
-      expect(this.list.offset(this.a)).toEqual(0);
-      expect(this.list.offset(this.b)).toEqual(3);
-      expect(this.list.offset(this.c)).toEqual(6);
+      expect(this.list.offset(this.a)).toBe(0);
+      expect(this.list.offset(this.b)).toBe(3);
+      expect(this.list.offset(this.c)).toBe(6);
     });
 
     it('forEach', function() {
       this.list.append(this.a, this.b, this.c);
       this.list.forEach(this.spy.callback);
-      expect(this.spy.callback.calls.count()).toEqual(3);
+      expect(this.spy.callback.calls.count()).toBe(3);
       var result = this.spy.callback.calls.all().reduce(function(memo, call) {
         return memo + call.args[0].str;
       }, '');
-      expect(result).toEqual('abc');
+      expect(result).toBe('abc');
     });
 
     it('map', function() {
@@ -180,26 +180,26 @@ describe('LinkedList', function() {
       var memo = this.list.reduce(function(memo, node) {
         return memo + node.str;
       }, '');
-      expect(memo).toEqual('abc');
+      expect(memo).toBe('abc');
     });
 
     it('forEachAt', function() {
       this.list.append(this.a, this.b, this.c);
       this.list.forEachAt(3, 3, this.spy.callback);
-      expect(this.spy.callback.calls.count()).toEqual(1);
+      expect(this.spy.callback.calls.count()).toBe(1);
       expect(this.spy.callback.calls.first().args).toEqual([this.b, 0, 3]);
     });
 
     it('forEachAt none', function() {
       this.list.append(this.a, this.b);
       this.list.forEachAt(1, 0, this.spy.callback);
-      expect(this.spy.callback.calls.count()).toEqual(0);
+      expect(this.spy.callback.calls.count()).toBe(0);
     });
 
     it('forEachAt partial nodes', function() {
       this.list.append(this.a, this.b, this.c);
       this.list.forEachAt(1, 7, this.spy.callback);
-      expect(this.spy.callback.calls.count()).toEqual(3);
+      expect(this.spy.callback.calls.count()).toBe(3);
       var calls = this.spy.callback.calls.all();
       expect(calls[0].args).toEqual([this.a, 1, 2]);
       expect(calls[1].args).toEqual([this.b, 0, 3]);
@@ -209,7 +209,7 @@ describe('LinkedList', function() {
     it('forEachAt at part of single node', function() {
       this.list.append(this.a, this.b, this.c);
       this.list.forEachAt(4, 1, this.spy.callback);
-      expect(this.spy.callback.calls.count()).toEqual(1);
+      expect(this.spy.callback.calls.count()).toBe(1);
       expect(this.spy.callback.calls.first().args).toEqual([this.b, 1, 1]);
     });
   });
