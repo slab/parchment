41ce8531f21b3ec33978e0d3a19f173ae173983f
Jason Chen <jhchen7@gmail.com>
2015-08-28 22:54:44 -0700

    add block tests

---
+++ test/unit/block.js
@@ -0,0 +1,31 @@
+xdescribe('Block', function() {
+  describe('format', function() {
+    it('add', function() {
+      var container = Registry.create('container');
+      var block = Registry.create('block');
+      container.appendChild(block);
+      block.format('header', 'h1');
+      expect(container.innerHTML).toBe('<h1></h1>');
+      expect(container.children.head.statics.blotName).toBe('header');
+      expect(container.children.head.getFormat()).toEqual({ header: 'h1' });
+    });
+
+    it('remove', function() {
+      var container = Registry.create('container');
+      var block = Registry.create('header', 'h1');
+      container.appendChild(block);
+      block.format('header', false);
+      expect(container.innerHTML).toBe('<p></p>');
+      expect(container.children.head.statics.blotName).toBe('block');
+    });
+
+    it('change', function() {
+      var container = Registry.create('container');
+      var block = Registry.create('header', 'h1');
+      block.format('header', 'h2');
+      expect(container.innerHTML).toBe('<h2></h2>');
+      expect(container.children.head.statics.blotName).toBe('header');
+      expect(container.children.head.getFormat()).toEqual({ header: 'h2' });
+    });
+  })
+});
