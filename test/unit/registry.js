d04c44683b086978e4a6b9240d051107e60fb5ff
Jason Chen <jhchen7@gmail.com>
2015-08-28 22:54:25 -0700

    add registry tests

--- test/unit/registry.js
+++ test/unit/registry.js
@@ -1,3 +1,38 @@
 describe('Registry', function() {
+  describe('create()', function() {
+    it('name', function() {
+      var blot = Registry.create('bold');
+      expect(blot instanceof BoldBlot).toBe(true);
+      expect(blot.statics.blotName).toBe('bold');
+    });
 
+    it('node', function() {
+      var node = document.createElement('strong');
+      var blot = Registry.create(node);
+      expect(blot instanceof BoldBlot).toBe(true);
+      expect(blot.statics.blotName).toBe('bold');
+    });
+
+    it('invalid', function() {
+      expect(function() {
+        Registry.create(BoldBlot);
+      }).toThrow();
+    });
+  });
+
+  describe('define()', function() {
+    it('invalid', function() {
+      expect(function() {
+        Registry.define({});
+      }).toThrow();
+    });
+  });
+
+  describe('match()', function() {
+    it('class', function() {
+      var node = document.createElement('em');
+      node.setAttribute('class', 'blot-bold');
+      expect(Registry.match(node)).toBe(BoldBlot);
+    });
+  });
 });
