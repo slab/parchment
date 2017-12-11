d22ff1b8fb7df045c65e0ef3cf1a1a720143c34d
Jason Chen <jhchen7@gmail.com>
2017-12-11 01:41:50 -0800

    fix test

--- test/unit/text.js
+++ test/unit/text.js
@@ -58,7 +58,7 @@ describe('TextBlot', function() {
     let before = textBlot.split(0);
     let after = textBlot.split(4);
     expect(before).toEqual(textBlot);
-    expect(after).toEqual(undefined);
+    expect(after).toBe(null);
   });
 
   it('split() force', function() {
